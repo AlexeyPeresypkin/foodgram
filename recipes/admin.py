50ea304e9237ce557cfa9744cce13316d7ec9505
Alexey Peresypkin <aepre3@gmail.com>
2022-01-06 19:36:48 +0300

    register apps in settings.py, edit recipes/models, register models in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from recipes.models import Recipe, Follow, Ingridient, RecipeIngredient
 
-admin.site.
+
+admin.site.register(Recipe)
+admin.site.register(Ingridient)
+admin.site.register(Follow)
+admin.site.register(RecipeIngredient)
+
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -11,8 +12,21 @@ User = get_user_model()
 
 
 class Ingridient(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Название')
-    dimension = models.CharField(max_length=250)
+    title = models.CharField(
+        max_length=250, verbose_name='Название'
+    )
+    dimension = models.CharField(
+        max_length=250,
+        verbose_name='Единица измерения'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Игридиенты'
 
 
 class Recipe(models.Model):
@@ -21,19 +35,77 @@ class Recipe(models.Model):
         ('LU', 'Обед'),
         ('DN', 'Ужин')
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    picture = models.ImageField(upload_to=user_directory_path)
-    description = models.TextField(verbose_name='Описание')
-    ingridients = models.ManyToManyField(Ingridient, through='Ingridients',
-                                         related_name='ingridients')
-    Tag = models.CharField(max_length=2, choices=TAGS)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='автор',
+                               related_name='recipes'
+                               )
+    title = models.CharField(
+        max_length=250, verbose_name='наименование'
+    )
+    image = models.ImageField(
+        upload_to=user_directory_path,
+        verbose_name='Изображение'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    ingridients = models.ManyToManyField(
+        Ingridient,
+        through='RecipeIngredient',
+        related_name='ingridients'
+    )
+    Tag = models.CharField(
+        max_length=2, choices=TAGS
+    )
     time_cooking = models.SmallIntegerField(
-        verbose_name='Время приготовления (мин)')
-    slug = models.SlugField(db_index=True, )
+        verbose_name='Время приготовления (мин)'
+    )
+    slug = models.SlugField(
+        db_index=True,
+    )
+    pub_date = models.DateField(
+        auto_now_add=True,
+        blank=True,
+        verbose_name='Дата публикации',
+    )
 
 
-class Ingridients(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingridients = models.ForeignKey(Ingridient, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingridient',
+    )
+    ingridient = models.ForeignKey(
+        Ingridient,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingridient',
+    )
+    quantity = models.SmallIntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name='Количество',
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='follower')  # подписчик
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='following')  # автор
+
+    def __str__(self):
+        return f'Автор: {self.author}, подписчик: {self.user}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'author'), name='unique_follow'
+            ),
+            models.CheckConstraint(
+                name='prevent_self_follow',
+                check=~models.Q(user=models.F('author'))
+            ),
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
